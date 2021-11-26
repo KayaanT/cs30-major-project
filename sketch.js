fdb8669c836732a548f515c1d708afa4e6d4f15d
KayaanT <kayaan.tharani@gmail.com>
2021-11-26 11:27:23 -0600

    tested function to move block down

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@
 
 let board;
 let cellSize;
+let tetris;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,12 +15,16 @@ function setup() {
   
   board = createEmptyBoard();
   console.log(board);
+
+  tetris = new Tetris();
+  tetris.createBlock();
 }
 
-function draw() {
+function draw() { 
   background(0);
   drawGrid();
-  createBlock();
+  tetris.fillBoard();
+  tetris.moveBlockDown();
 }
 
 function createEmptyBoard() {
@@ -44,20 +49,40 @@ function drawGrid() {
   }
 }
 
-function createBlock() {
-  let block = createEmptyBoard();
 
-  block[0][0] = 1;
-  block[1][0] = 1;
-  block[2][0] = 1;
 
-  let buffer = width/2 - 10*cellSize/2;
-  for (let y = 0; y < block.length; y++) {
-    for (let x = 0; x < block[y].length; x++) {
-      if (block[y][x] === 1) {
-        fill("red");
-        rect(x*cellSize+buffer, y*cellSize, cellSize, cellSize);
+class Tetris {
+  constructor() {
+    this.block;
+  }
+
+  createBlock() {
+    this.block = createEmptyBoard();
+  
+    this.block[0][0] = 1;
+    this.block[1][0] = 1;
+    this.block[2][0] = 1;  
+  }
+
+  fillBoard() {
+    let buffer = width/2 - 10*cellSize/2;
+    for (let y = 0; y < this.block.length; y++) {
+      for (let x = 0; x < this.block[y].length; x++) {
+        if (this.block[y][x] === 1) {
+          fill("red");
+          rect(x*cellSize+buffer, y*cellSize, cellSize, cellSize);
+        }
       }
     }
   }
+
+  moveBlockDown() {
+    for (let i = 0; i < 17; i++) { 
+      this.block[i][0] = 0;
+      this.block[i + 1][0] = 1;
+      this.block[i + 2][0] = 1;
+      this.block[i + 3][0] = 1;
+    }
+  }
 }
+
