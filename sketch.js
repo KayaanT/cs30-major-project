042dbe041fa31c133fb31db21eae41d872256fca
KayaanT <kayaan.tharani@gmail.com>
2021-11-29 12:11:39 -0600

    created 5 tetris blocks

--- sketch.js
+++ sketch.js
@@ -53,21 +53,49 @@ function drawGrid() {
 
 class Tetris {
   constructor() {
-    this.block;
+    this.block = createEmptyBoard();
   }
 
   createBlock() {
-    this.whichBlock = 1;
+    this.whichBlock = 6;
+
     if (this.whichBlock === 1) {
-      this.block = createEmptyBoard();
-    
       this.block[0][0] = 1;
       this.block[1][0] = 1;
       this.block[2][0] = 1;  
     }
 
     else if (this.whichBlock === 2) {
-      return;
+      this.block[0][0] = 1;
+      this.block[1][0] = 1;
+      this.block[1][1] = 1; 
+      this.block[0][1] = 1; 
+      
+    }
+
+    else if (this.whichBlock === 3) {
+      this.block[0][0] = 1;
+      this.block[1][0] = 1;
+      this.block[1][1] = 1;
+      this.block[1][2] = 1;
+    }
+    else if (this.whichBlock === 4) {
+      this.block[0][2] = 1;
+      this.block[1][0] = 1;
+      this.block[1][1] = 1;
+      this.block[1][2] = 1;
+    }
+    else if (this.whichBlock === 5) {
+      this.block[0][0] = 1;
+      this.block[0][1] = 1;
+      this.block[1][1] = 1;
+      this.block[1][2] = 1;
+    }
+    else if (this.whichBlock === 6) {
+      this.block[1][0] = 1;
+      this.block[0][1] = 1;
+      this.block[1][1] = 1;
+      this.block[2][1] = 1;
     }
   }
 
@@ -84,14 +112,69 @@ class Tetris {
   }
 
   moveBlockDown(i) {
-    this.block[i][0] = 0;
-    this.block[i + 1][0] = 1;
-    this.block[i + 2][0] = 1;
-    this.block[i + 3][0] = 1;
+    if (this.whichBlock === 1) {
+      this.block[i][0] = 0;
+      this.block[i + 1][0] = 1;
+      this.block[i + 2][0] = 1;
+      this.block[i + 3][0] = 1;
+    }
+    else if (this.whichBlock === 2) {
+      this.block[i][0] = 0;
+      this.block[i+1][0] = 1;
+      this.block[i+2][0] = 1;
+
+      this.block[i][1] = 0;
+      this.block[i+1][1] = 1;
+      this.block[i+2][1] = 1;
+    }
+    else if (this.whichBlock === 3) {
+      this.block[i][0] = 0;
+      this.block[i+1][0] = 1;
+      this.block[i+2][0] = 1;
+
+      this.block[i+1][1] = 0;
+      this.block[i+2][1] = 1;
+
+      this.block[i+1][2] = 0;
+      this.block[i+2][2] = 1;
+    }
+    else if (this.whichBlock === 4) {
+      this.block[i][2] = 0;
+      this.block[i+1][2] = 1;
+      this.block[i+2][2] = 1;
+
+      this.block[i][0] = 0;
+      this.block[i+1][0] = 1;
+
+      this.block[i][1] = 0;
+      this.block[i+1][1] = 1;
+    }
+    else if (this.whichBlock === 5) {
+      this.block[i][0] = 0;
+      this.block[i+1][0] = 1;
+
+      this.block[i][1] = 0;
+      this.block[i+1][1] = 1;
+      this.block[i+2][1] = 1;
+
+      this.block[i+1][2] = 0;
+      this.block[i+2][2] = 1;
+    }
+    else if (this.whichBlock === 6) {
+      this.block[i+1][0] = 0;
+      this.block[i+2][0] = 1;
+
+      this.block[i][1] = 0;
+      this.block[i+1][1] = 1;
+      this.block[i+2][1] = 1;
+
+      this.block[i][2] = 0;
+      this.block[i+1][2] = 1;
+    }
   }
 
   callMoveBlockDown(current) {
-    for (this.i = 0; this.i < 17; this.i++) {
+    for (this.i = 0; this.i < 15; this.i++) {
       this.moveBlockDown(this.i);
     }
   }
